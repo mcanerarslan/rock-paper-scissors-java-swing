fc473974ae283daded90adf9bc0e6265eb151704
M.Caner Arslan <mahmutcanerarslan@gmail.com>
2026-03-10 20:33:30 +0300

    first commit

---
+++ src/rockpaperscissors/Game.java
@@ -0,0 +1,92 @@
+package rockpaperscissors;
+
+import java.util.Random;
+
+public class Game {
+	
+	Random random = new Random();
+
+	private int playerStats = 0, botStats = 0;
+	private String selectionOfBot;
+
+	private int generateBotChoice() {
+
+		int number = random.nextInt(3) + 1;
+		return number;
+	}
+
+	public void playGame(int playerSelection) {
+
+		int botSelection = generateBotChoice();
+		saveSelectionOfBot(botSelection);
+
+		// 1 : Rock
+		// 2 : Paper
+		// 3 : Scissors
+
+//		System.out.println("User: " + playerSelection);
+//		System.out.println("Bot: " + getSelectionOfBot());
+
+		if (playerSelection == botSelection) {
+//			System.out.println("Berabere");
+		} else if ((playerSelection == 1 && botSelection == 3) || (playerSelection == 2 && botSelection == 1)
+				|| (playerSelection == 3 && botSelection == 2)) {
+//			System.out.println("Oyuncu kazandi");
+			playerStats++;
+		} else {
+//			System.out.println("Bot kazandi");
+			botStats++;
+		}
+
+	}
+	
+	public void restartGame() {
+		playerStats = 0;
+		botStats = 0;
+		selectionOfBot = null;
+	}
+	
+	public void saveGame() {
+		
+	}
+
+	public String saveSelectionOfBot(int value) {
+		switch (value) {
+		case 1:
+			this.selectionOfBot = "rock";
+			break;
+		case 2:
+			this.selectionOfBot = "paper";
+			break;
+		case 3:
+			this.selectionOfBot = "scissors";
+			break;
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + value);
+		}
+		return "save";
+	}
+	
+	
+
+	public String getSelectionOfBot() {
+		return selectionOfBot;
+	}
+
+	public int getPlayerStats() {
+		return playerStats;
+	}
+
+	public void setPlayerStats(int playerStats) {
+		this.playerStats = playerStats;
+	}
+
+	public int getBotStats() {
+		return botStats;
+	}
+
+	public void setBotStats(int botStats) {
+		this.botStats = botStats;
+	}
+
+}
---
+++ src/rockpaperscissors/Main.java
@@ -0,0 +1,318 @@
+package rockpaperscissors;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	private static boolean isThemeLight = true;
+
+	private static final int ROCK = 1;
+	private static final int PAPER = 2;
+	private static final int SCISSORS = 3;
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+
+		JFrame menuFrame = new JFrame("Rock Paper Scissors");
+
+
+		// LABELS
+		// =========================
+		JLabel userStatsJLabel = new JLabel("User: 0");
+		userStatsJLabel.setBounds(10, 0, 150, 30);
+
+		JLabel botStatsJLabel = new JLabel("Bot: 0");
+		botStatsJLabel.setBounds(10, 20, 150, 30);
+
+		JLabel showPlayerSelection = new JLabel();
+		showPlayerSelection.setBounds(170, 150, 64, 64);
+
+		JLabel showBotSelection = new JLabel();
+		showBotSelection.setBounds(240, 150, 64, 64);
+
+
+		// MENU BAR
+		// =========================
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu menu = new JMenu("Game");
+		JMenu options = new JMenu("Options");
+
+		JMenuItem start = new JMenuItem("Start");
+		JMenuItem restart = new JMenuItem("Restart");
+		JMenuItem save = new JMenuItem("Save");
+
+		JMenuItem about = new JMenuItem("About");
+		JMenuItem theme = new JMenuItem("Theme");
+		JMenuItem exit = new JMenuItem("Exit");
+
+		menu.add(start);
+		menu.add(restart);
+		menu.add(save);
+
+		options.add(about);
+		options.add(theme);
+
+		menuBar.add(menu);
+		menuBar.add(options);
+		menuBar.add(exit);
+
+
+		// BUTTONS
+		// =========================
+		JButton rockbtn = new JButton("Taş");
+		rockbtn.setBounds(154, 350, 64, 64);
+
+		JButton paperbtn = new JButton("Kağıt");
+		paperbtn.setBounds(218, 350, 64, 64);
+
+		JButton scissorsbtn = new JButton("Makas");
+		scissorsbtn.setBounds(282, 350, 64, 64);
+
+		JButton startButton = new JButton();
+		startButton.setBounds(150, 150, 200, 200);
+
+
+		// LOAD IMAGES
+		// =========================
+		URL rockUrl = Main.class.getResource("/rockpaperscissors/assets/rock.png");
+		URL paperUrl = Main.class.getResource("/rockpaperscissors/assets/paper.png");
+		URL scissorsUrl = Main.class.getResource("/rockpaperscissors/assets/scissors.png");
+		URL startUrl = Main.class.getResource("/rockpaperscissors/assets/rock-paper-scissors.png");
+
+		if (rockUrl != null) {
+			rockbtn.setIcon(new ImageIcon(rockUrl));
+		} else {
+			System.out.println("rock.png bulunamadi");
+		}
+
+		if (paperUrl != null) {
+			paperbtn.setIcon(new ImageIcon(paperUrl));
+		} else {
+			System.out.println("paper.png bulunamadi");
+		}
+
+		if (scissorsUrl != null) {
+			scissorsbtn.setIcon(new ImageIcon(scissorsUrl));
+		} else {
+			System.out.println("scissors.png bulunamadi");
+		}
+
+		if (startUrl != null) {
+			startButton.setIcon(new ImageIcon(startUrl));
+		} else {
+			System.out.println("rock-paper-scissors.png bulunamadi");
+		}
+
+
+		// BUTTON APPEARANCE
+		// =========================
+		rockbtn.setBorderPainted(false);
+		rockbtn.setContentAreaFilled(false);
+		rockbtn.setFocusPainted(false);
+
+		paperbtn.setBorderPainted(false);
+		paperbtn.setContentAreaFilled(false);
+		paperbtn.setFocusPainted(false);
+
+		scissorsbtn.setBorderPainted(false);
+		scissorsbtn.setContentAreaFilled(false);
+		scissorsbtn.setFocusPainted(false);
+
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setFocusPainted(false);
+
+
+		// INITIAL VISIBILITY
+		// =========================
+		rockbtn.setVisible(false);
+		paperbtn.setVisible(false);
+		scissorsbtn.setVisible(false);
+
+
+		// MENU ACTIONS
+		// =========================
+		about.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null,
+						"Rock Paper Scissors\nMade by Mahmut Caner Arslan\nGitHub: mcanerarslan");
+			}
+		});
+
+		theme.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isThemeLight) {
+					menuFrame.getContentPane().setBackground(Color.BLACK);
+					userStatsJLabel.setForeground(Color.WHITE);
+					botStatsJLabel.setForeground(Color.WHITE);
+					isThemeLight = false;
+				} else {
+					menuFrame.getContentPane().setBackground(Color.WHITE);
+					userStatsJLabel.setForeground(Color.BLACK);
+					botStatsJLabel.setForeground(Color.BLACK);
+					isThemeLight = true;
+				}
+			}
+		});
+
+		restart.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.restartGame();
+				userStatsJLabel.setText("User: 0");
+				botStatsJLabel.setText("Bot: 0");
+				showPlayerSelection.setIcon(null);
+				showBotSelection.setIcon(null);
+			}
+		});
+
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.saveGame();
+				JOptionPane.showMessageDialog(menuFrame, "Game saved.");
+			}
+		});
+
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.saveGame();
+				System.exit(0);
+			}
+		});
+
+		start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rockbtn.setVisible(true);
+				paperbtn.setVisible(true);
+				scissorsbtn.setVisible(true);
+				startButton.setVisible(false);
+			}
+		});
+
+
+		// GAME BUTTON ACTIONS
+		// =========================
+		rockbtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.playGame(ROCK);
+				userStatsJLabel.setText("User: " + game.getPlayerStats());
+				botStatsJLabel.setText("Bot: " + game.getBotStats());
+				showPlayerSelection.setIcon(new ImageIcon(rockUrl));
+
+				String botChoice = game.getSelectionOfBot();
+
+				switch (botChoice) {
+				case "rock":
+					showBotSelection.setIcon(new ImageIcon(rockUrl));
+					break;
+				case "paper":
+					showBotSelection.setIcon(new ImageIcon(paperUrl));
+					break;
+				case "scissors":
+					showBotSelection.setIcon(new ImageIcon(scissorsUrl));
+					break;
+				}
+			}
+		});
+
+		paperbtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.playGame(PAPER);
+				userStatsJLabel.setText("User: " + game.getPlayerStats());
+				botStatsJLabel.setText("Bot: " + game.getBotStats());
+				showPlayerSelection.setIcon(new ImageIcon(paperUrl));
+
+				String botChoice = game.getSelectionOfBot();
+
+				switch (botChoice) {
+				case "rock":
+					showBotSelection.setIcon(new ImageIcon(rockUrl));
+					break;
+				case "paper":
+					showBotSelection.setIcon(new ImageIcon(paperUrl));
+					break;
+				case "scissors":
+					showBotSelection.setIcon(new ImageIcon(scissorsUrl));
+					break;
+				}
+			}
+		});
+
+		scissorsbtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.playGame(SCISSORS);
+				userStatsJLabel.setText("User: " + game.getPlayerStats());
+				botStatsJLabel.setText("Bot: " + game.getBotStats());
+				showPlayerSelection.setIcon(new ImageIcon(scissorsUrl));
+
+				String botChoice = game.getSelectionOfBot();
+
+				switch (botChoice) {
+				case "rock":
+					showBotSelection.setIcon(new ImageIcon(rockUrl));
+					break;
+				case "paper":
+					showBotSelection.setIcon(new ImageIcon(paperUrl));
+					break;
+				case "scissors":
+					showBotSelection.setIcon(new ImageIcon(scissorsUrl));
+					break;
+				}
+			}
+		});
+
+		startButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rockbtn.setVisible(true);
+				paperbtn.setVisible(true);
+				scissorsbtn.setVisible(true);
+				startButton.setVisible(false);
+			}
+		});
+
+
+		// ADD COMPONENTS TO FRAME
+		// =========================
+		menuFrame.setJMenuBar(menuBar);
+		menuFrame.add(userStatsJLabel);
+		menuFrame.add(botStatsJLabel);
+		menuFrame.add(showPlayerSelection);
+		menuFrame.add(showBotSelection);
+		menuFrame.add(rockbtn);
+		menuFrame.add(paperbtn);
+		menuFrame.add(scissorsbtn);
+		menuFrame.add(startButton);
+
+
+		// FRAME SETTINGS
+		// =========================
+		menuFrame.setSize(500, 500);
+		menuFrame.setLayout(null);
+		menuFrame.setResizable(false);
+		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menuFrame.setVisible(true);
+	}
+}
